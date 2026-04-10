548d31c8d8da9f51d7c656df5181d131e5000214
Ubuntu <ubuntu@ip-172-31-32-193.ap-south-1.compute.internal>
2026-04-10 13:22:25 +0000

    INITIAL COMMIT

---
+++ node.js
@@ -0,0 +1,7 @@
+const express=require('express');
+const app=express();
+app.get('/', (req,res)=>{
+	res.send("Hello DevOps World!");
+});
+app.listen(3000,()=>
+	console.log("Script running on port 30000")
